414f67208f30b13acce4d2d0e703a82bd0c68e29
nfischer <nadav.fischer@capriza.com>
2016-03-11 15:00:08 +0200

    move push multi commands into push lua script + make sure all keys get expired upon update

--- lib/lua/ack.lua
+++ lib/lua/ack.lua
@@ -1,7 +1,8 @@
 -- KEYS[1] - the 'waiting for ack' queue
 -- KEYS[2] - the messages queue
 -- ARGV[1] - the last message id that was acked
--- ARGV[2] - 'true' means to restore un-acked messages back to the messages queue
+-- ARGV[2] - keys ttl
+-- ARGV[3] - 'true' means to restore un-acked messages back to the messages queue
 
 -- continously pop messages up to the last acked message and discard the acked ones
 local lastack = tonumber(ARGV[1])
@@ -15,6 +16,7 @@ while cont do
     else
         -- the current message id is not yet acked, put the id back to the queue
         redis.call('lpush',KEYS[1], id)
+        redis.call('expire', KEYS[1], ARGV[2])
         cont = false  -- break out from the loop
     end
 end
@@ -22,13 +24,14 @@ end
 -- if we want to restore all the un-acked messages back to the messages queue, then
 -- we pop the messages from the end of the 'waiting for ack' queue and push them the head of the messages queue
 -- so that they can be consumed once again
-if ARGV[2] == 'true' then
+if ARGV[3] == 'true' then
     while true do
         -- pop the next last element from the 'waiting for ack' queue
         local element = redis.call('rpop',KEYS[1])
         if not element then break end
         -- push it to the head of messages queue
         redis.call('lpush',KEYS[2], element)
+        redis.call('expire', KEYS[2], ARGV[2])
     end
 end
 
